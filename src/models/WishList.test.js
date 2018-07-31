fd98e4890a91e8837856ec73f3ade649b704e758
ZestHo <zest6789tower@gmail.com>
2018-07-31 10:09:48 +0800

    snapshot and patch

--- src/models/WishList.test.js
+++ src/models/WishList.test.js
@@ -1,3 +1,4 @@
+import { getSnapshot, onSnapshot, onPatch } from 'mobx-state-tree';
 import { WishListItem, WishList } from "./WishList";
 
 it('can create a instance of model', () => {
@@ -29,17 +30,78 @@ it('can create a wishList', () => {
     expect(list.items[0].price).toBe(28.73);
 });
 
-it('can add a wish', () => {
-    const list = WishList.create();
-    list.add(
-        WishListItem.create({
-            name: 'Chesterton',
-            price: 10
-        })
-    );
+// it('can add a wish', () => {
+//     const list = WishList.create();
+//     /**
+//      * every time the list changes somehow and a new snapshot gets produced,
+//      * because it changed, it just pushes onto the state list.
+//      */
+//     const states = [];
+//     onSnapshot(list, snapshot => {
+//         states.push(snapshot);
+//     });
 
-    expect(list.items.length).toBe(1);
-    expect(list.items[0].name).toBe('Chesterton');
-    list.items[0].changeName('G.K');
-    expect(list.items[0].name).toBe('G.K');
+//     list.add(
+//         // WishListItem.create({
+//         //     name: 'Chesterton',
+//         //     price: 10
+//         // })
+//     // 因為在wishList中已經定義了type, 所以就算你只是放入object，wishList也知道你放了什麼
+//         {
+//             name: 'Chesterton',
+//             price: 10
+//         }
+//     );
+
+//     expect(list.items.length).toBe(1);
+//     expect(list.items[0].name).toBe('Chesterton');
+//     expect(list.items[0].price).toBe(10);
+    
+//     list.items[0].changeName('G.K');
+//     expect(list.items[0].name).toBe('G.K');
+    
+//     list.items[0].changePrice(20);
+//     expect(list.items[0].price).toBe(20);
+//     /**
+//      * getSnapshot(): which is basically the inverse process of what create does.
+//      * getStructure will produce an immutable data structure, which uses structural sharing, so, it's very cheap.
+//      *
+//      * deep equality check
+//      */
+//     expect(getSnapshot(list)).toEqual({
+//         items: [
+//             {
+//                 name: 'G.K',
+//                 price: 20,
+//                 image: ''
+//             }
+//         ]
+//     });
+//     /**
+//      * 跟以上是同等意思
+//      * A snapshot is an immutable representation of the entire model tree, and they generated automatically
+//      * in the background
+//      */
+//     expect(getSnapshot(list)).toMatchSnapshot();
+
+//     expect(states).toMatchSnapshot();
+// });
+
+it('can add a wish - 2', () => {
+    const list = WishList.create();
+    
+    const patches = [];
+    onPatch(list, patch => {
+        patches.push(patch);
+    });
+
+    list.add({
+        name: 'Chesterton',
+        price: 10
+    });
+
+    list.items[0].changePrice(20);
+    expect(list.items[0].price).toBe(20);
+
+    expect(patches).toMatchSnapshot();
 });
